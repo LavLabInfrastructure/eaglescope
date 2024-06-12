e9975d44309e6ea529b4688724a574e3cac2c139
default <mjbarrett@mcw.edu>
2024-06-12 15:31:58 +0000

    fix linting

--- source/components/DownloadButton.js
+++ source/components/DownloadButton.js
@@ -11,50 +11,51 @@ class DownloadButton extends PureComponent {
       data = this.props.data[1];
     }
     // JSON
-    if (format == 'json'){
-    const blob = new Blob([JSON.stringify(data)], { type: 'application/json' });
-    const href = URL.createObjectURL(blob);
-    const jsonlink = document.createElement('a');
-    jsonlink.href = href;
-    jsonlink.download = this.props.title || 'download.json';
-    document.body.appendChild(jsonlink);
-    jsonlink.click();
-    document.body.removeChild(jsonlink);
-    URL.revokeObjectURL(href);       
+    if (format == 'json') {
+      const blob = new Blob([JSON.stringify(data)], { type: 'application/json' });
+      const href = URL.createObjectURL(blob);
+      const jsonlink = document.createElement('a');
+      jsonlink.href = href;
+      jsonlink.download = this.props.title || 'download.json';
+      document.body.appendChild(jsonlink);
+      jsonlink.click();
+      document.body.removeChild(jsonlink);
+      URL.revokeObjectURL(href);
     } else {
-     // CSV
-    let csvContent = 'data:text/csv;charset=utf-8,';
-    let i = 0;
-    data.forEach((rowArray) => {
-      if (i == 0) {
-        const keyArray = Object.keys(rowArray);
-        const header = keyArray.join(',');
-        csvContent += `${header}\n`;
-        i++;
-      }
-    const valueArray = Object.values(rowArray);
-    const row = valueArray.join(',');
-    csvContent += `${row}\n`;
-    });
-    const encodedUri = encodeURI(csvContent);
-    const csvlink = document.createElement('a');
-    csvlink.href = encodedUri;
-    csvlink.download = this.props.title || 'download.csv';
-    document.body.appendChild(csvlink);
-    csvlink.click();
-    document.body.removeChild(csvlink);
-    } 
+      // CSV
+      let csvContent = 'data:text/csv;charset=utf-8,';
+      let i = 0;
+      data.forEach((rowArray) => {
+        if (i == 0) {
+          const keyArray = Object.keys(rowArray);
+          const header = keyArray.join(',');
+          csvContent += `${header}\n`;
+          i++;
+        }
+        const valueArray = Object.values(rowArray);
+        const row = valueArray.join(',');
+        csvContent += `${row}\n`;
+      });
+      const encodedUri = encodeURI(csvContent);
+      const csvlink = document.createElement('a');
+      csvlink.href = encodedUri;
+      csvlink.download = this.props.title || 'download.csv';
+      document.body.appendChild(csvlink);
+      csvlink.click();
+      document.body.removeChild(csvlink);
+    }
   }
 
   render() {
     return (
-      <DropdownButton 
-        title={ <FontAwesomeIcon size="1x" icon="download" />}
-        id={this.id} 
-        size="lg" 
-        style={{background: 'none', border: 'none'}}>
-              <Dropdown.Item onClick={() => this.download('csv')} > Download as CSV</Dropdown.Item>
-              <Dropdown.Item onClick={() => this.download('json')} > Download as JSON</Dropdown.Item>
+      <DropdownButton
+        title={<FontAwesomeIcon size="1x" icon="download" />}
+        id={this.id}
+        size="lg"
+        style={{ background: 'none', border: 'none' }}
+      >
+        <Dropdown.Item onClick={() => this.download('csv')}> Download as CSV</Dropdown.Item>
+        <Dropdown.Item onClick={() => this.download('json')}> Download as JSON</Dropdown.Item>
       </DropdownButton>
     );
   }
